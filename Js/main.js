092915544bcd4b02aef1731af2d28e283654a0b8
Adrian-1405 <adrian-weber@live.de>
2024-06-18 13:11:07 +0200

    Version V3

---
+++ Js/main.js
@@ -0,0 +1,67 @@
+/*=============== SEARCH ===============*/
+
+
+/*=============== LOGIN ===============*/
+
+
+/*=============== ADD SHADOW HEADER ===============*/
+
+
+/*=============== HOME SWIPER ===============*/
+let swiperHome = new Swiper('.home__swiper', {
+  loop: true,
+  spaceBetween: -24,
+  grabCursor: true,
+  slidesPerView: 'auto',
+  centeredSlides: 'auto',
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    1220: {
+      slidesPerView:4,
+      centeredSlides:false,
+    }
+  }
+})
+
+/*=============== FEATURED SWIPER ===============*/
+
+let swiperItembox2 = new Swiper('.itembox2__swiper', {
+  loop: true,
+  spaceBetween: 16,
+  grabCursor: true,
+  slidesPerView: 'auto',
+  centeredSlides: 'auto',
+
+  autoplay: {
+    delay: 8000,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    1120: {
+  
+    }
+  }
+})
+
+/*=============== NEW SWIPER ===============*/
+
+
+/*=============== TESTIMONIAL SWIPER ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
